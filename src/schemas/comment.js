6e11dd4111c4c325209427422e8792f4b9bd6310
Skitsouprenup <goustlz@gmail.com>
2022-12-17 17:21:22 +0800

    app schemas for sanity.io

---
+++ src/schemas/comment.js
@@ -0,0 +1,23 @@
+export default {
+    
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
+    fields: [
+        {
+            name: 'commentId',
+            title: 'CommentId',
+            type: 'string',
+        },
+        {
+            name: 'postedBy',
+            title: 'PostedBy',
+            type: 'postedBy',
+        },
+        {
+            name: 'comment',
+            title: 'Comment',
+            type: 'string',
+        },
+    ]
+}
---
+++ src/schemas/user.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'username',
+            title: 'Username',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string',
+        },
+        {
+            name: 'banner',
+            title: 'Banner',
+            type: 'image',
+        },
+    ]
+}
